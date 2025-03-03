ba0a7039de70b28e17970dc5f39ab00596da6faf
VincentKon <vincent373kon@gmail.com>
2025-03-03 16:44:09 +0700

    disable typescript for the time being

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import PatientForm from "@/components/forms/PatientForm";
 import PasskeyModel from "@/components/PasskeyModel";
